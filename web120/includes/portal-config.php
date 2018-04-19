43d04eb2da43152ff6c3e49f9c5c0307b268e9fc
Bill Newman <william.newman@seattlecolleges.edu>
2018-04-19 00:21:05 -0700

    2nd upload to repo
    Hopefully the 2nd commit picks up the change to the config file!

--- web120/includes/portal-config.php
+++ web120/includes/portal-config.php
@@ -3,17 +3,9 @@
 portal-config.php
 
 Used to store all of our WEB120 configuration information
-
 */
 
-//echo basename($_SERVER['PHP_SELF']);
-
 define('THIS_PAGE',basename($_SERVER['PHP_SELF']));
-
-//echo THIS_PAGE;
-
-//die;
-
 
 switch(THIS_PAGE){
 
